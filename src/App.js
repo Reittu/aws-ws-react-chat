7cd40621b0ee1eace8ddd73762fa46dfeb6a9b9e
Reittu <Tony.Reittu@gmail.com>
2020-03-22 23:21:50 +0200

    Setup backend (AWS) & prep. frontend WS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+
+http://websocket.org/echo.html
+wss://mt57ra0zm9.execute-api.us-east-1.amazonaws.com/dev
+{"message": "test", "action": "message"}
+
+----
+
+const awsSocket = new WebSocket("wss://mt57ra0zm9.execute-api.us-east-1.amazonaws.com/dev");
+awsSocket.onopen = () => console.log("Socket opened");
+awsSocket.onclose = () => console.log("Socket closed");
+awsSocket.onmessage = (event) => console.log(event.data);
+awsSocket.onerror = (error) => console.log("Error occured", error);
+
+awsSocket.send(JSON.stringify({"message": "test2", "action": "message"}));
+
+*/
+
 function App() {
   return (
     <div className="App">
